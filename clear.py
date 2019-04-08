4f9c391b835dec437c30c722c26c7ac43a309c7d
shtryler <hans@straile.net>
2019-04-08 18:47:10 +0200

    all the new lamps

--- clear.py
+++ clear.py
@@ -15,8 +15,8 @@ def main():
         mosi=Pin(16),
         sck=Pin(17)
     )
-    black = "0"*6*40
-    white = "f"*6*40
+    black = "0"*6*80
+    white = "f"*6*80
 
     d = Display(spi, 8, 5)
     d.set_data(white)
--- random.py
+++ random.py
@@ -16,7 +16,7 @@ def main(program):
     )
     sleep_us(200000)
 
-    d = Display(spi, 8, 5)
+    d = Display(spi, 12, 5)
     g = Glow(d)
 
     pixels = []
