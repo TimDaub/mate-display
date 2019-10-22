af618762818fb1e11c6b2a9c68621a4a9df8d7bc
shtryler <hans@straile.net>
2019-10-22 09:42:21 +0200

    clear now accepts color as argument

--- clear.py
+++ clear.py
@@ -4,7 +4,7 @@ from utime import sleep_us
 from display import Display
 
 # A program to clean the screen in a stylish manner
-def main():
+def main(hexcolor):
     sleep_us(500000)
     spi = SPI(
         1,
@@ -16,16 +16,16 @@ def main():
         sck=Pin(17)
     )
     black = "0"*6*100
-    white = "f"*6*100
+    color = hexcolor*100
 
     d = Display(spi, 20, 5)
-    d.set_data(white)
+    d.set_data(color)
     d.show()
     sleep_us(50000)
     d.set_data(black)
     d.show()
     sleep_us(50000)
-    d.set_data(white)
+    d.set_data(color)
     d.show()
     sleep_us(50000)
     d.set_data(black)
--- gitupdate.py
+++ gitupdate.py
@@ -3,10 +3,22 @@ import urequests
 
 def main():
     path = "https://raw.githubusercontent.com/TimDaub/mate-display/master/"
+    #update filelist first
+    filename = "filelist.txt"
+    response = urequests.get(path + filename)
+    code = open(filename, "w")
+    code.write(response.text)
+    code.close()
+    print(filenamscre)
     filelist = open("filelist.txt", "r")
     for line in filelist:
+        #update files from list
         filename = str.rstrip(line)
+        if filename[:1] == "#":
+            pass
         response = urequests.get(path + filename)
+        if response[:3] == "404":
+            pass
         code = open(filename, "w")
         code.write(response.text)
         code.close()
